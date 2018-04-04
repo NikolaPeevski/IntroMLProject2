ce4dac0bb402668ca3ae00ab8d54cc0a9907f0ce
Nikola Peevski <Jeratull@hotmail.com>
2018-04-04 09:12:06 +0200

    Add Lambda cross validation Add figures structure

--- mainp2.py
+++ mainp2.py
@@ -3,11 +3,17 @@ import pandas as pd
 import utils as utils
 
 utils.clearConsole()
+
 dataSet = utils.loadDataSet()
+
 X = dataSet.values[:, :24] #Prefferably
+
 y = dataSet.values[:, np.newaxis, 24] #Prefferably
+
 attributeNames = list(dataSet) #Attribute titles, used for plotting
+
 K = 10 #Number of folds
 
 #Simple crossValidation with 10 folds
-utils.crossValidation(X, y, attributeNames, K)
+#utils.crossValidation(X, y, attributeNames, K)
+utils.lambdaOptimalRegulation(X,y,attributeNames)
--- utils.py
+++ utils.py
@@ -165,8 +165,64 @@ def bmplot(yt, xt, X):
     for i in range(0,len(xt)):
         axvline(i-0.5, color='black')
 
+def rlr_validate(X,y,lambdas,cvf=10):
+    ''' Validate regularized linear regression model using 'cvf'-fold cross validation.
+        Find the optimal lambda (minimizing validation error) from 'lambdas' list.
+        The loss function computed as mean squared error on validation set (MSE).
+        Function returns: MSE averaged over 'cvf' folds, optimal value of lambda,
+        average weight values for all lambdas, MSE train&validation errors for all lambdas.
+
+        Parameters:
+        X       training data set
+        y       vector of values
+        lambdas vector of lambda values to be validated
+        cvf     number of crossvalidation folds     
+        
+        Returns:
+        opt_val_err         validation error for optimum lambda
+        opt_lambda          value of optimal lambda
+        mean_w_vs_lambda    weights as function of lambda (matrix)
+        train_err_vs_lambda train error as function of lambda (vector)
+        test_err_vs_lambda  test error as function of lambda (vector)
+    '''
+    from sklearn import model_selection
+    CV = model_selection.KFold(cvf)
+    M = X.shape[1]
+    w = np.empty((M,cvf,len(lambdas)))
+    train_error = np.empty((cvf,len(lambdas)))
+    test_error = np.empty((cvf,len(lambdas)))
+    f = 0
+    for train_index, test_index in CV.split(X,y):
+        X_train = X[train_index]
+        y_train = y[train_index]
+        X_test = X[test_index]
+        y_test = y[test_index]
+        
+        # precompute terms
+        Xty = X_train.T @ y_train
+        XtX = X_train.T @ X_train
+
+        for l in range(0,len(lambdas)):
+            # Compute parameters for current value of lambda and current CV fold
+            # note: "linalg.lstsq(a,b)" is substitue for Matlab's left division operator "\"
+            w[:,f,l] = linalg.lstsq(XtX+lambdas[l] * np.eye(M),Xty)[0].squeeze()
+            # Evaluate training and test performance
+            train_error[f,l] = np.power(y_train-X_train @ w[:,f,l].T,2).sum()/y_train.shape[0]
+            test_error[f,l] = np.power(y_test-X_test @ w[:,f,l].T,2).sum()/y_test.shape[0]
+    
+        f=f+1
+
+    opt_val_err = np.min(np.mean(test_error,0))
+    opt_lambda = lambdas[np.argmin(np.mean(test_error,0))]
+    train_err_vs_lambda = np.mean(train_error,0)
+    test_err_vs_lambda = np.mean(test_error,0)
+    mean_w_vs_lambda = np.squeeze(np.mean(w,1))
+    
+    return opt_val_err, opt_lambda, mean_w_vs_lambda, train_err_vs_lambda, test_err_vs_lambda
+
+
 def crossValidation(X, y, attributeNames, K = 10):
-    ''' '''
+    ''' Simple cross validation for an X matrix and y target'''
     print(X)
     print(y)
     N, M = X.shape
@@ -274,5 +330,101 @@ def crossValidation(X, y, attributeNames, K = 10):
                   
         
     show()
+
+def lambdaOptimalRegulation(X, y, attributeNames):
+    N, M = X.shape
+
+    # Add offset attribute
+    X = np.concatenate((np.ones((X.shape[0],1)),X),1)
+    attributeNames = [u'Offset']+attributeNames
+    M = M+1
+
+    ## Crossvalidation
+    # Create crossvalidation partition for evaluation
+    K = 5
+    CV = model_selection.KFold(K)
+
+    # Values of lambda
+    lambdas = np.power(10.,range(-5,9))
+
+    # Initialize variables
+    #T = len(lambdas)
+    Error_train = np.empty((K,1))
+    Error_test = np.empty((K,1))
+    Error_train_rlr = np.empty((K,1))
+    Error_test_rlr = np.empty((K,1))
+    Error_train_nofeatures = np.empty((K,1))
+    Error_test_nofeatures = np.empty((K,1))
+    w_rlr = np.matrix(np.empty((M,K)))
+    w_noreg = np.matrix(np.empty((M,K)))
+
+    k = 0
+
+    for train_index, test_index in CV.split(X,y):
     
+        # extract training and test set for current CV fold
+        X_train = X[train_index]
+        y_train = y[train_index]
+        X_test = X[test_index]
+        y_test = y[test_index]
+        internal_cross_validation = 10    
+        
+        opt_val_err, opt_lambda, mean_w_vs_lambda, train_err_vs_lambda, test_err_vs_lambda = rlr_validate(X_train, y_train, lambdas, internal_cross_validation)
+
+        Xty = X_train.T @ y_train
+        XtX = X_train.T @ X_train
+        
+        # Compute mean squared error without using the input data at all
+        Error_train_nofeatures[k] = np.square(y_train-y_train.mean()).sum()/y_train.shape[0]
+        Error_test_nofeatures[k] = np.square(y_test-y_test.mean()).sum()/y_test.shape[0]
+
+        # Estimate weights for the optimal value of lambda, on entire training set
+        w_rlr[:,k] = np.linalg.lstsq(XtX+opt_lambda*np.eye(M),Xty)[0]
+        # Compute mean squared error with regularization with optimal lambda
+        Error_train_rlr[k] = np.square(y_train-X_train @ w_rlr[:,k]).sum()/y_train.shape[0]
+        Error_test_rlr[k] = np.square(y_test-X_test @ w_rlr[:,k]).sum()/y_test.shape[0]
+
+        # Estimate weights for unregularized linear regression, on entire training set
+        w_noreg[:,k] = np.linalg.lstsq(XtX,Xty)[0]
+        # Compute mean squared error without regularization
+        Error_train[k] = np.square(y_train-X_train @ w_noreg[:,k]).sum()/y_train.shape[0]
+        Error_test[k] = np.square(y_test-X_test @ w_noreg[:,k]).sum()/y_test.shape[0]
+        # OR ALTERNATIVELY: you can use sklearn.linear_model module for linear regression:
+        #m = lm.LinearRegression().fit(X_train, y_train)
+        #Error_train[k] = np.square(y_train-m.predict(X_train)).sum()/y_train.shape[0]
+        #Error_test[k] = np.square(y_test-m.predict(X_test)).sum()/y_test.shape[0]
+
+        figure(k, figsize=(12,8))
+        subplot(1,2,1)
+        semilogx(lambdas,mean_w_vs_lambda.T,'.-')
+        xlabel('Regularization factor')
+        ylabel('Mean Coefficient Values')    
+        
+        subplot(1,2,2)
+        title('Optimal lambda = {0}'.format(opt_lambda))
+        loglog(lambdas,train_err_vs_lambda.T,'b.-',lambdas,test_err_vs_lambda.T,'r.-')
+        xlabel('Regularization factor')
+        ylabel('Squared error (crossvalidation)')
+        legend(['Train error','Validation error'])
+        
+        print('Cross validation fold {0}/{1}:'.format(k+1,K))
+        print('Train indices: {0}'.format(train_index))
+        print('Test indices: {0}\n'.format(test_index))
+
+        k+=1
+
+    # Display results
+    print('\n')
+    print('Linear regression without feature selection:\n')
+    print('- Training error: {0}'.format(Error_train.mean()))
+    print('- Test error:     {0}'.format(Error_test.mean()))
+    print('- R^2 train:     {0}'.format((Error_train_nofeatures.sum()-Error_train.sum())/Error_train_nofeatures.sum()))
+    print('- R^2 test:     {0}\n'.format((Error_test_nofeatures.sum()-Error_test.sum())/Error_test_nofeatures.sum()))
+    print('Regularized Linear regression:')
+    print('- Training error: {0}'.format(Error_train_rlr.mean()))
+    print('- Test error:     {0}'.format(Error_test_rlr.mean()))
+    print('- R^2 train:     {0}'.format((Error_train_nofeatures.sum()-Error_train_rlr.sum())/Error_train_nofeatures.sum()))
+    print('- R^2 test:     {0}\n'.format((Error_test_nofeatures.sum()-Error_test_rlr.sum())/Error_test_nofeatures.sum()))
+
+    show()
 
